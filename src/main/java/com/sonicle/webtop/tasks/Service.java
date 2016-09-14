8ae11194ec4c583e090e841255330330faa81267
Matteo Albinola <malbinola@sonicle.com>
2016-09-14 09:34:06 +0200

    RIP ClientOptions, Long file ServiceVars

--- src/main/java/com/sonicle/webtop/tasks/Service.java
+++ src/main/java/com/sonicle/webtop/tasks/Service.java
@@ -124,8 +124,8 @@ public class Service extends BaseService {
 	}
 	
 	@Override
-	public ClientOptions returnClientOptions() {
-		ClientOptions co = new ClientOptions();
+	public ServiceVars returnServiceVars() {
+		ServiceVars co = new ServiceVars();
 		return co;
 	}
 	
