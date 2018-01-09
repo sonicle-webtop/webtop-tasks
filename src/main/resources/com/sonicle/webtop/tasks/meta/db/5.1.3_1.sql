45eb0a50ee5aa5a7f7c8fb7617dee23095e8dec9
Matteo Albinola <malbinola@sonicle.com>
2018-01-09 12:30:28 +0100

    Add settings migration

--- src/main/resources/com/sonicle/webtop/tasks/meta/db/5.1.3_1.sql
+++ src/main/resources/com/sonicle/webtop/tasks/meta/db/5.1.3_1.sql
@@ -26,6 +26,23 @@ CREATE INDEX "category_props_ak1" ON "tasks"."category_props" USING btree ("cate
 ALTER TABLE "tasks"."category_props" ADD PRIMARY KEY ("domain_id", "user_id", "category_id");
 
 -- ----------------------------
+-- Data move: user_settings -> category_props
+-- ----------------------------
+INSERT INTO "tasks"."category_props" 
+("domain_id", "user_id", "category_id", "hidden", "color") 
+SELECT domain_id, user_id, 
+CAST (replace("key",'category.folder.data@','') AS int4), 
+CASE WHEN split_part(split_part(substr("value",2,length("value")-2),',',1),':',2)='true' THEN TRUE ELSE NULL END, 
+CASE WHEN replace(split_part(split_part(substr("value",2,length("value")-2),',',2),':',2),'"','')='null' THEN NULL ELSE replace(split_part(split_part(substr("value",2,length("value")-2),',',2),':',2),'"','') END 
+FROM "core"."user_settings" 
+WHERE "service_id" = 'com.sonicle.webtop.tasks' AND "key" LIKE 'category.folder.data@%';
+
+-- ----------------------------
+-- Cleanup possibly orphans
+-- ----------------------------
+DELETE FROM "tasks"."category_props" WHERE "category_id" NOT IN (SELECT "category_id" from "tasks"."categories");
+
+-- ----------------------------
 -- Cleanup old settings
 -- ----------------------------
 @DataSource[default@com.sonicle.webtop.core]
