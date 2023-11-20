adb95d85ed4e6ffce572bbee2972a3e7f9d63a52
Matteo Albinola <malbinola@sonicle.com>
2023-11-20 12:04:30 +0100

    Remove "WITH (OIDS=FALSE)" from sql files adding support to PSQL12

--- src/main/resources/com/sonicle/webtop/tasks/meta/db/5.6.0_1.sql
+++ src/main/resources/com/sonicle/webtop/tasks/meta/db/5.6.0_1.sql
@@ -6,10 +6,7 @@
 CREATE TABLE "tasks"."tasks_tags" (
 "task_id" int4 NOT NULL,
 "tag_id" varchar(22) NOT NULL
-)
-WITH (OIDS=FALSE)
-
-;
+);
 
 CREATE INDEX "tasks_tags_ak1" ON "tasks"."tasks_tags" USING btree ("tag_id");
 ALTER TABLE "tasks"."tasks_tags" ADD PRIMARY KEY ("task_id", "tag_id");
@@ -26,10 +23,7 @@ CREATE TABLE "tasks"."tasks_custom_values" (
 "boolean_value" bool,
 "date_value" timestamptz(6),
 "text_value" text
-)
-WITH (OIDS=FALSE)
-
-;
+);
 
 ALTER TABLE "tasks"."tasks_custom_values" ADD PRIMARY KEY ("task_id", "custom_field_id");
 ALTER TABLE "tasks"."tasks_custom_values" ADD FOREIGN KEY ("task_id") REFERENCES "tasks"."tasks" ("task_id") ON DELETE CASCADE ON UPDATE CASCADE;
