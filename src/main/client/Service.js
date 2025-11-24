4da9961c2aeb318ea8f637e470b96e3ad57fe310
malbinola <malbinola@sonicle.com>
2025-11-24 14:31:45 +0100

    Fix grid-items selected/over BG in light/dark theme

--- src/main/client/Service.js
+++ src/main/client/Service.js
@@ -244,7 +244,7 @@ Ext.define('Sonicle.webtop.tasks.Service', {
 				}, {
 					xtype: 'sotreepanel',
 					reference: 'trfolders',
-					cls: 'wttasks-tool-tree',
+					cls: 'wt-tool-tree wttasks-tool-tree',
 					bodyCls: 'wt-tool-bg',
 					border: false,
 					useArrows: true,
